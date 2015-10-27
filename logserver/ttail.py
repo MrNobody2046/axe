e979e1fb7b542aeb9a0f96295471867256a03984
kenny <sphy@foxmail.com>
2015-10-27 23:06:52 +0800

    fixed

--- logserver/ttail.py
+++ logserver/ttail.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python
 import time
-from subprocess import *
-import argparse
+import sys
+from subprocess import Popen,PIPE
 import fcntl
 import select
 import os
 import Queue
 
 
-class File(object):
+class FReader(object):
+    """
+    """
     NORMAL = 1
     SSH = 2
 
-    def __init__(self, name):
-        self.name = name
-        if name.find(":") != -1:
-            host, remote_file = fd.split(':', 1)
-            self.basename = os.path.basename(remote_file)
-            self.fd = Popen(["ssh", "-o BatchMode=yes", host, "tail", "-f", remote_file],
-                            stdin=PIPE, stdout=PIPE, close_fds=True)
-            self._t = self.SSH
-        else:
-            self._t = self.NORMAL
-            self.fd = open(name)
-            self.basename = os.path.basename(name)
+    def __init__(self, path):
+        """
+        :param path:
+        :return:
+        """
+        self.path = path
+        self.abs_path = os.path.abspath(path)
+        {True: self.connect_remote_file, False: self.connect_local_file}[path.find(":") != -1](path)
         self._buffer = ""
         self.queue = Queue.Queue()
+        self.line = []
+
+    def connect_remote_file(self, path):
+        host, remote_file = path.split(':', 1)
+        self.basename = os.path.basename(remote_file)
+        self.fd = Popen(["ssh", "-o BatchMode=yes", host, "tail", "-f", remote_file],
+                        stdin=PIPE, stdout=PIPE, close_fds=True)
+        self._t = self.SSH
+
+    def connect_local_file(self, path):
+        self._t = self.NORMAL
+        self.fd = open(path)
+        self.basename = os.path.basename(path)
 
     @property
     def file_name(self):
@@ -34,54 +45,74 @@ class File(object):
     def __hash__(self):
         return self.fd.__hash__()
 
-    def _read_line(self):
-        line = ''
-        fileno = self.fd.fileno()
-        fl = fcntl.fcntl(fileno, fcntl.F_GETFL)
-        fcntl.fcntl(fileno, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+    def read_line(self):
+        self._buffer = ""
+        fd = self.fd.fileno()
+        fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
         try:
             self._buffer += self.fd.read(1)
             while self._buffer[-1] != '\n':
                 self._buffer += self.fd.read(1)
-            fcntl.fcntl(fileno, fcntl.F_SETFL, fl)
             return True
         except IOError:
-            fcntl.fcntl(fileno, fcntl.F_SETFL, fl)
             return False
+        finally:
+            fcntl.fcntl(fd, fcntl.F_SETFL, fl)
 
     def read(self):
-        if self._t != self.NORMAL:
-            pipe = self.fd
-            pipe.poll()
-        if self._read_line():
+        if self.is_ssh_file:
+            self.fd.poll()
+        if self.read_line():
             return self._buffer
         else:
             return ""
 
+    @property
+    def is_ssh_file(self):
+        return self._t == self.SSH
 
 class CH(object):
     def __init__(self, m):
         self.m = m
 
     def __hash__(self):
-        return self.m.__hash__()
+        return 1
 
 
-a = 1
-s = "sadasd"
-print a.__hash__()
-print s.__hash__()
+class Tail(object):
+    def __init__(self):
+        self.fd2freader_dict = {}
+        self._fd_list = []
 
-someset = {CH(1), CH(2), CH(333)}
-print CH(1) in someset
-print someset
-1 / 0
+    def add_freader(self, freader):
+        assert isinstance(freader, FReader)
+        self.fd2freader_dict[freader.fd] = freader
+        self._fd_list.append(freader.fd)
+
+    def run(self):
+        print "Start", self._fd_list
+        while len(self.fd2freader_dict) > 0:
+            rlist, _, _ = select.select(self._fd_list, [], [],1)
+            for read_file in rlist:
+                freader = self.fd2freader_dict[read_file]
+                buf = freader.read()
+                if buf:
+                    sys.stdout.write(buf)
+            # print rlist
 
 
-class LineStream():
-    def __init__(self, files):
-        self.files = files
-        self.file_list = [f.fd for f in files]
+# t = Tail()
+# t.add_freader(FReader("./x"))
+# t.run()
+#
+# 1 / 0
+
+
+class LineStream(object):
+    def __init__(self, file_dict):
+        self.files = file_dict
+        self.file_list = file_dict.keys()
 
     def run(self):
         buffers = {}
@@ -91,35 +122,35 @@ class LineStream():
             print rlist, _, _
             wait = 0
             for readfile in rlist:
-                readfile = self.files[readfile]
-                if isinstance(readfile, basestring):
-                    newline, full = self.readline(readfile)
-                    if readfile in buffers:
-                        buffers[readfile] += newline
+                fd = readfile
+                if isinstance(fd, basestring):
+                    newline, full = self.readline(fd)
+                    if fd in buffers:
+                        buffers[fd] += newline
                     else:
-                        buffers[readfile] = newline
-                    if buffers[readfile] and full:
-                        print "%s::%s" % (self.files[readfile],
-                                          buffers[readfile]),
-                        del buffers[readfile]
-                    elif not buffers[readfile] and wait == 0:
+                        buffers[fd] = newline
+                    if buffers[fd] and full:
+                        print "%s::%s" % (self.files[fd],
+                                          buffers[fd]),
+                        del buffers[fd]
+                    elif not buffers[fd] and wait == 0:
                         wait = 1
                 else:
                     # for ssh pipes
                     wait = 2
-                    pipe = self.files[readfile][0]
-                    pipe.poll()
-                    newline, full = self.readline(readfile)
-                    if readfile in buffers:
-                        buffers[readfile] += newline
-                    else:
-                        buffers[readfile] = newline
-                    if buffers[readfile] and full:
-                        print "%s::%s" % (self.files[readfile][1],
-                                          buffers[readfile]),
-                        del buffers[readfile]
-                    if pipe.returncode:
-                        del self.files[readfile]
+                    # pipe = self.files[fd][0]
+                    # pipe.poll()
+                    # newline, full = self.readline(readfile)
+                    # if readfile in buffers:
+                    #     buffers[readfile] += newline
+                    # else:
+                    #     buffers[readfile] = newline
+                    # if buffers[readfile] and full:
+                    #     print "%s::%s" % (self.files[readfile][1],
+                    #                       buffers[readfile]),
+                    #     del buffers[readfile]
+                    # if pipe.returncode:
+                    #     del self.files[readfile]
             if wait == 1:
                 time.sleep(30.1)
 
@@ -151,34 +182,34 @@ class LineStream():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='Multitail remote and local files.')
-    parser.add_argument('logfiles', nargs='+', metavar='FILE',
-                        help='Logfiles to follow')
-    parser.add_argument('-g', '--grep', action='store', metavar='REGEXP',
-                        help='Show only the lines that match the regexp REGEXP')
-    params = parser.parse_args()
+    # parser = argparse.ArgumentParser(
+    #     description='Multitail remote and local files.')
+    # parser.add_argument('logfiles', nargs='+', metavar='FILE',
+    #                     help='Logfiles to follow')
+    # parser.add_argument('-g', '--grep', action='store', metavar='REGEXP',
+    #                     help='Show only the lines that match the regexp REGEXP')
+    # params = parser.parse_args()
+    #
+    # grepstring = params.grep or ''
+    #
+    # read_files = {}
+    # for logfile in params.logfiles:
+    #     if logfile.find(':') >= 0:
+    #         host, logfile = logfile.split(':', 1)
+    #         p = Popen(["ssh", "-o BatchMode=yes", host, "tail", "-f", logfile],
+    #                   stdin=PIPE, stdout=PIPE, close_fds=True)
+    #         read_files[p.stdout] = [p, host + ':' + logfile]
+    #     else:
+    #         fd = open(logfile, 'r')
+    #         read_files[fd] = logfile
+    # try:
+    #     reader = LineStream(read_files)
+    #     reader.run()
+    # except KeyboardInterrupt:
+    #     reader.close()
 
-    grepstring = params.grep or ''
+    file_dict = {
+        open("x"): "x"
+    }
 
-    read_files = {}
-    for logfile in params.logfiles:
-        if logfile.find(':') >= 0:
-            host, logfile = logfile.split(':', 1)
-            p = Popen(["ssh", "-o BatchMode=yes", host, "tail", "-f", logfile],
-                      stdin=PIPE, stdout=PIPE, close_fds=True)
-            read_files[p.stdout] = [p, host + ':' + logfile]
-        else:
-            fd = open(logfile, 'r')
-            read_files[fd] = logfile
-    try:
-        reader = LineStream(read_files)
-        reader.run()
-    except KeyboardInterrupt:
-        reader.close()
-
-file_dict = {
-    open("x"): "x"
-}
-
-LineStream(file_dict).run()
+    LineStream(file_dict).run()
